05d4f7dfeff316135561f189d4742fc894823a14
ngochnn <49011220+ngochnn@users.noreply.github.com>
2020-04-23 12:23:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    
+    <h1>Xác định tam giác</h1>
+    <form action="loaitamgiac.php" method="get">
+        <br>Nhập cạnh a
+        <input type="text" name="a">
+        <br>Nhập cạnh b
+        <input type="text" name="b">
+        <br>Nhập cạnh c
+        <input type="text" name="c">
+        <br>
+        <input type="submit" name="Caculate" value="Đáp án">
+        
+    </form>
+
+    <h1>Xác định ngày kế tiếp</h1>
+    <form action="ngayketiep.php" method="get">
+        <br>Nhập ngày hiện tại
+        <input type="text" name="day">
+        <br>Nhập tháng hiện tại
+        <input type="text" name="month">
+        <br>Nhập năm hiện tại
+        <input type="text" name="year">
+        <br>
+        <input type="submit" name="Caculate2" value="Đáp án" >
+        
+    </form>
+</body>
+</html>
